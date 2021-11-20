c15fbeca82ba1eb3c1f701ca0dcbebeb42721355
s-elo <zz321yy@qq.com>
2021-11-20 22:21:27 +0800

    timer finished

--- demos/01-timer/script/index.ts
+++ demos/01-timer/script/index.ts
@@ -1,7 +1,53 @@
-import {t} from './g/test.js';
+const timeDom = {
+  Day: document.querySelector("#day"),
+  Hour: document.querySelector("#hour"),
+  Minute: document.querySelector("#minute"),
+  Second: document.querySelector("#second"),
+};
 
-const port = 3500;
-const domain = `http://localhost:${port}`;
+const date = new Date(`1 Jan 2022`);
 
-console.log(domain, t);
+type Time = {
+  Day: string | number;
+  Hour: string | number;
+  Minute: string | number;
+  Second: string | number;
+};
 
+function render(time: Time) {
+  Object.entries(time).forEach(([key, value]) => {
+    (timeDom as any)[key].innerHTML = value;
+  });
+}
+
+function formator(time: Time) {
+  const formatedTime: Time = { ...time };
+
+  Object.entries(time).forEach(([key, value]) => {
+    if (value < 10) {
+      (formatedTime as any)[key] = `0${value}`;
+    }
+  });
+
+  return formatedTime;
+}
+
+function createTime() {
+  const diff = (date.getTime() - Date.now()) / 1000;
+
+  const Day = Math.floor(diff / (3600 * 24));
+  const Hour = Math.floor((diff % (3600 * 24)) / 3600);
+  const Minute = Math.floor((diff % 3600) / 60);
+  const Second = Math.ceil(diff % 60);
+
+  render(
+    formator({
+      Day,
+      Hour,
+      Minute,
+      Second,
+    })
+  );
+}
+
+setInterval(createTime, 1000);
