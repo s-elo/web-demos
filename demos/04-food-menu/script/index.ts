7216e4001fccb36f8c96b243ed4dd604f9d4f182
s-elo <zz321yy@qq.com>
2021-12-09 11:53:04 +0800

    04 random menu and fav menu; add and remove fav menu

--- demos/04-food-menu/script/index.ts
+++ demos/04-food-menu/script/index.ts
@@ -1,6 +1,7 @@
 import { request } from "../../utils.js";
 
 const menuListDom = document.querySelector(".menu-list") as HTMLElement;
+const favListDom = document.querySelector(".fav-list ul");
 
 interface MenuType {
   strMealThumb: string;
@@ -8,9 +9,57 @@ interface MenuType {
   strMeal: string;
 }
 
-function getFavList() {}
+(async () => {
+  const meals = (await getRandomMeals(2)) as Array<{ meals: Array<MenuType> }>;
 
-function getMealById(id: string) {
+  meals.forEach((meal) => {
+    renderRandomMeal(meal.meals[0]);
+  });
+
+  const favMeals = (await getFavList()) as Array<{ meals: Array<MenuType> }>;
+  console.log(favMeals);
+
+  favMeals.forEach((meal) => {
+    renderFavMeal(meal.meals[0]);
+  });
+})();
+
+function addToFavList(mealId: string) {
+  const favMeals = getFavIds();
+
+  favMeals.push(mealId);
+  localStorage.setItem("fav-meals", JSON.stringify(favMeals));
+}
+
+function removeFromFavList(mealId: string) {
+  const favMeals = getFavIds();
+  localStorage.setItem(
+    "fav-meals",
+    JSON.stringify(favMeals.filter((id) => id !== mealId))
+  );
+}
+
+function getFavIds() {
+  const favMealStr = localStorage.getItem("fav-meals");
+
+  return JSON.parse(favMealStr ? favMealStr : "[]") as Array<string>;
+}
+
+async function getFavList() {
+  const ids = getFavIds();
+
+  return Promise.all(
+    ids.map((id) => {
+      return request({
+        url: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,
+        method: "GET",
+        remoteURL: true,
+      });
+    })
+  );
+}
+
+function getMealById(mealId: string) {
   // www.themealdb.com/api/json/v1/1/lookup.php?i=52772
 }
 
@@ -32,24 +81,60 @@ async function getRandomMeals(num: number) {
   return Promise.all(mealRequests);
 }
 
-async function renderRandomMeal(meal: MenuType) {
-  const menuHTML = `
-  <section class="menu">
+function renderRandomMeal(meal: MenuType) {
+  const menuDom = document.createElement("section");
+  menuDom.classList.add("menu");
+
+  const isFavMeal = getFavIds().includes(meal.idMeal);
+
+  menuDom.innerHTML = `
   <h4 class="tag">Random Menu</h4>
   <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
   <div class="menu-info">
     <div class="menu-name">${meal.strMeal}</div>
-    <button class="fav-btn"><i class="far fa-heart"></i></button>
-  </div>
-</section>`;
+    <button class="fav-btn"><i class="${
+      isFavMeal ? "fas" : "far"
+    } fa-heart"></i></button>
+  </div>`;
 
-  menuListDom?.insertAdjacentHTML("beforeend", menuHTML);
+  // the menuDom can be treated as a dom already
+  // but not being mount
+  const favBtnDom = menuDom.querySelector(".menu-info .fav-btn");
+
+  favBtnDom?.addEventListener("click", () => {
+    const favHeart = favBtnDom.firstChild as HTMLElement;
+
+    if ([...favHeart.classList].includes("far")) {
+      favHeart.classList.remove("far");
+      favHeart.classList.add("fas");
+      addToFavList(meal.idMeal); // add
+
+      renderFavMeal(meal);
+    } else {
+      favHeart.classList.remove("fas");
+      favHeart.classList.add("far");
+      removeFromFavList(meal.idMeal); // remove
+
+      removeFavMeal(meal.idMeal);
+    }
+  });
+
+  menuListDom?.appendChild(menuDom);
 }
 
-(async () => {
-  const meals = (await getRandomMeals(2)) as Array<{ meals: Array<MenuType> }>;
+function renderFavMeal(meal: MenuType) {
+  const favMenuHTML = `
+  <li id="fav-${meal.idMeal}">
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+    <div class="fav-menu-name" title="${meal.strMeal}">${meal.strMeal}</div>
+  </li>
+  `;
 
-  meals.forEach((meal) => {
-    renderRandomMeal(meal.meals[0]);
-  });
-})();
+  favListDom?.insertAdjacentHTML("beforeend", favMenuHTML);
+}
+
+function removeFavMeal(mealId: string) {
+  const favMealDom = favListDom?.querySelector(`#fav-${mealId}`);
+
+  favMealDom?.parentNode?.removeChild(favMealDom);
+}
